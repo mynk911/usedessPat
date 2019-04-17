84e7e919f3bbf5dfa4d46c6bf5481c34d459f796
mayank bansal <bansalmayank63@gmail.com>
2019-04-17 06:59:04 +0530

    GFG : modified interface

--- usedessPat.cpp
+++ usedessPat.cpp
@@ -1,3 +1,7 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -65,7 +69,7 @@ int main (int argc, char *argv[])
 #ifdef CS180_FOUND
     {
         std::cout << "stable matching algo :" << std::endl;
-        cs180::stable_matching();
+        //cs180::stable_matching();
         cs180::pq::heap<int> pq;
         pq.insert(5, 1);
         pq.insert(4, 8);
@@ -82,252 +86,319 @@ int main (int argc, char *argv[])
 #ifdef GFG_FOUND
   //get number of test cases
   int t, scn;;
-  int A[1010]; int B[1010];
+  int A[1010]; int B[1010]; int ress[1010];
+  size_t res_arr[1010];
   printf("maximum_subarray : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      printf("%d\n", maximum_subarray(A, n));
+      int res;
+      maximum_subarray(A, n, &res);
+      printf("%d\n", res);
   }
   printf("find_missing_number : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      printf("%d\n", find_missing_number(A, n));
+      int res;
+      find_missing_number(A, n, &res);
+      printf("%d\n", res);
   }
   printf("subarry_with_given_sum : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       int s; scn = scanf("%d", &s);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      int res[2];
-      subarry_with_given_sum(A, n, s, res, 2);
-      printf("%d %d\n", res[0], res[1]);
+      size_t res[2];
+      if(!subarray_with_given_sum(A, n, s, res, 2))
+        printf("%d %d\n", res[0], res[1]);
+      else
+        printf("-1\n");
   }
   printf("sort 0 1 2 : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
       sort_012(A, n);
+      for (size_t i = 0; i < n; i++)
+          printf("%d ", A[i]);
+      printf("\n");
   }
   printf("equilibrium point : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      equilibrium_point(A, n);
+      size_t res;
+      equilibrium_point(A, n, &res);
+      printf("%u\n", res);
   }
   printf("maximum sum increasing subsequence : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%u", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      maximum_sum_increasing_subsequence(A, n);
+      int res;
+      maximum_sum_increasing_subsequence(A, n, &res);
+      printf("%d\n", res);
   }
   printf("leaders in an array : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      leaders_in_an_array(A, n);
+      size_t res_size;
+      leaders_in_an_array(A, n, B, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", B[i]);
+      printf("\n");
   }
   printf("no of platforms : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &B[i]);
-      minimum_platforms(A, B, n);
+      int res;
+      minimum_platforms(A, B, n, &res);
+      printf("%d\n", res);
   }
   printf("maximums_of_subarray_size_k : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
-      int k; scn = scanf("%d", &k);
+      size_t n; scn = scanf("%d", &n);
+      size_t k; scn = scanf("%d", &k);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      maximums_of_subarray_size_k(A, n, k);
+      size_t res_size;
+      maximums_of_subarray_size_k(A, n, k, B, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", B[i]);
+      printf("\n");
   }
   printf("reverse_array_in_groups : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
-      int k; scn = scanf("%d", &k);
+      size_t n; scn = scanf("%d", &n);
+      size_t k; scn = scanf("%d", &k);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
       reverse_array_in_groups(A, n, k);
+      for (size_t i = 0; i < n; i++)
+          printf("%d ", A[i]);
+      printf("\n");
   }
   printf("kth_smallest_element : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      int k; scn = scanf("%d", &k);
-      kth_smallest_element(A, n, k);
+      size_t k; scn = scanf("%d", &k);
+      int res;
+      kth_smallest_element(A, n, k, &res);
+      printf("%d\n", res);
   }
   printf("trapping_rainwater : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      trapping_rainwater(A, n);
+      long long res;
+      trapping_rainwater(A, n, &res);
+      printf("%lld\n", res);
   }
   printf("pythagorean_triplet : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
       pythagorean_triplet(A, n);
+      for (size_t i = 0; i < n; i++)
+          printf("%d ", A[i]);
+      printf("\n");
   }
   printf("chocolate_distribution : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      int m; scn = scanf("%d", &m);
-      chocolate_distribution(A, n, m);
+      size_t m; scn = scanf("%d", &m);
+      int res;
+      chocolate_distribution(A, n, m, &res);
+      printf("%d\n", res);
   }
   printf("stock_buy_and_sell : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      stock_buy_and_sell(A, n);
+      size_t res_size;
+      stock_buy_and_sell(A, n, res_arr, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%u ", res_arr[i]);
+      printf("\n");
+
   }
   printf("elements_with_left_side_smaller_and_right_side_greater : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      elements_with_left_side_smaller_and_right_side_greater(A, n);
+      int res;
+      elements_with_left_side_smaller_and_right_side_greater(A, n, &res);
+      printf("%d\n", res);
   }
   printf("convert_array_into_zigzag_fashion : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
       convert_array_into_zigzag_fashion(A, n);
+      for (size_t i = 0; i < n; i++)
+          printf("%d ", A[i]);
+      printf("\n");
   }
   printf("find_the_element_that_appears_once_in_sorted_array : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      find_the_element_that_appears_once_in_sorted_array(A, n);
+      int res;
+      find_the_element_that_appears_once_in_sorted_array(A, n, &res);
+      printf("%d\n", res);
   }
   printf("kth_largest_element_in_a_stream : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int k; scn = scanf("%d", &k);
-      int n; scn = scanf("%d", &n);
+      size_t k; scn = scanf("%d", &k);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      kth_largest_element_in_a_stream(A, n,k);
+      size_t res_size;
+      kth_largest_element_in_a_stream(A, n, k, B, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", B[i]);
+      printf("\n");
   }
   printf("relative_sorting : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
-      int m; scn = scanf("%d", &m);
+      size_t n; scn = scanf("%d", &n);
+      size_t m; scn = scanf("%d", &m);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%d", &A[i]);
-      for (int i = 0; i < m; i++)
+      for (size_t i = 0; i < m; i++)
           scn = scanf("%d", &B[i]);
-      relative_sorting(A, B, n, m);
+      size_t res_size;
+      relative_sorting(A, n, B, m, ress, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", ress[i]);
+      printf("\n");
   }
   printf("spirally_traversing_a_matrix : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int m; scn = scanf("%d", &m);
-      int n; scn = scanf("%d", &n);
+      size_t m; scn = scanf("%d", &m);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < m; i++)
-          for(int j = 0; j < n; j++)
+      for (size_t i = 0; i < m; i++)
+          for(size_t j = 0; j < n; j++)
           scn = scanf("%d", &A[n*i + j]);
-      spirally_traversing_a_matrix(A, m, n);
+      size_t res_size;
+      spirally_traversing_a_matrix(A, m, n, B, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", B[i]);
+      printf("\n");
   }
   printf("sorting_array_elements_by_frequency : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
               scn = scanf("%d", &A[i]);
-      sorting_array_elements_by_frequency(A, n);
+      size_t res_size;
+      sorting_array_elements_by_frequency(A, n, B, &res_size);
+      for (size_t i = 0; i < res_size; i++)
+          printf("%d ", B[i]);
+      printf("\n");
   }
   struct arr As[105];
   printf("largest_number_formed_by_array : \n");
   scn = scanf("%d", &t);
   while (t--) {
       //get the size of array
-      int n; scn = scanf("%d", &n);
+      size_t n; scn = scanf("%d", &n);
       //get array entries
-      for (int i = 0; i < n; i++)
+      for (size_t i = 0; i < n; i++)
           scn = scanf("%s", As[i].a);
-      largest_number_formed_by_array(As, n);
+      char str[50];
+      size_t res_size;
+      largest_number_formed_by_array(As, n, str, &res_size);
+      printf("%s\n", str);
   }
 #endif // GFG
 
